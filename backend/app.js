b3278de1957c886d9ddb937a46fa742017f57101
juliah90 <137844881+juliah90@users.noreply.github.com>
2024-02-16 10:41:31 -0800

    first commit

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority`)
+.then(()=>{
+   console.log('Successfully connected to mongodb Atlas');
+})
+.catch((error)=>{
+   console.log('Unable to connect to Mongodb Atlas');
+   console.error(error);
+})
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+ });
+
+app.use((req, res) => {
+   res.json({ message: 'Your request was successful!' }); 
+});
+
+module.exports = app;
